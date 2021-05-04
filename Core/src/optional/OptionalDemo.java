b33e50030532edd04a6dceb928612e5ba01575cb
anujRaka <anuj.factiousboy@gmail.com>
2021-05-04 19:53:12 +0530

    Add Java Optional.ofNullable() example

--- Core/src/optional/OptionalDemo.java
+++ Core/src/optional/OptionalDemo.java
@@ -15,5 +15,35 @@ public class OptionalDemo {
 
         Optional<Map<String, String>> optionalMap = Optional.ofNullable(testMap);
         System.out.println(optionalMap.isPresent());
+
+        String str = "test string";
+        String derivedStr1 = Optional.ofNullable(str).orElse("other value");
+        str = null;
+        String derivedStr2 = Optional.ofNullable(str).orElse("other value");
+        System.out.println(derivedStr1 + " || " + derivedStr2);
+
+
+        OptionalDemoSample ods1 = new OptionalDemoSample("ods1");
+        OptionalDemoSample ods2 = new OptionalDemoSample("ods2");
+
+        OptionalDemoSample derivedODS1 = Optional.ofNullable(ods1).orElse(ods2);
+        ods1 = null;
+        OptionalDemoSample derivedODS2 = Optional.ofNullable(ods1).orElse(ods2);
+        System.out.println(derivedODS1 + " || " + derivedODS2);
+    }
+}
+
+class OptionalDemoSample {
+    private String name;
+
+    public OptionalDemoSample(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "OptionalDemoSample{" +
+                "name='" + name + '\'' +
+                '}';
     }
 }
